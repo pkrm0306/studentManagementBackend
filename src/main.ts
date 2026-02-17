e747367434cb5d7505187613b22741a5c0abb9dc
Prabhas Miraki <prabhasr@mirakitech.com>
2026-02-17 18:54:52 +0530

    deployement in render

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ async function bootstrap() {
   });
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port,'0.0.0.0');
   console.log(`Application is running on: http://localhost:${port}`);
   console.log(`Swagger documentation is available at: http://localhost:${port}/api`);
 }
